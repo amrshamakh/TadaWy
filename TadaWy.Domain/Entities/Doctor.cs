0eb71917f43ce201e3c4506dd3fa58bcbd5470ff
amrshamakh <eissaamr308@gmail.com>
2026-02-04 17:55:56 +0200

    initial commit

---
+++ TadaWy.Domain/Entities/Doctor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TadaWy.Domain.Entities
+{
+    public class Doctor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Experience { get; set; }
+        public bool IsApproved { get; set; }
+        public string Specialization { get; set; }
+        
+        public int rating { get; set; }
+
+    }
+}
