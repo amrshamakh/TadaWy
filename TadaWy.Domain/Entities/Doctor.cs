798d06ad9a293e99b39861248af1bba3cfb205a7
amrshamakh <eissaamr308@gmail.com>
2026-02-05 15:26:47 +0200

    add Doctor entity with Reviews and Schedules

--- TadaWy.Domain/Entities/Doctor.cs
+++ TadaWy.Domain/Entities/Doctor.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TadaWy.Domain.ValueObjects;
 
 namespace TadaWy.Domain.Entities
 {
@@ -10,11 +11,14 @@ namespace TadaWy.Domain.Entities
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Experience { get; set; }
         public bool IsApproved { get; set; }
         public string Specialization { get; set; }
-        
-        public int rating { get; set; }
+        public Address Address { get; set; } = default!;
+        public double rating { get; set; }
 
+        public ICollection<Appointment> Appointments { get; private set; }= new List<Appointment>();
+
+        public ICollection<DoctorSchedule> Schedules { get; private set; }= new List<DoctorSchedule>();
+        public ICollection<DoctorReview> Reviews { get; private set; }= new List<DoctorReview>();
     }
 }
---
+++ TadaWy.Domain/Entities/DoctorReview.cs
@@ -0,0 +1,12 @@
+﻿namespace TadaWy.Domain.Entities
+{
+    public class DoctorReview
+    {
+        public int Id { get; set; }
+        public int DoctorId { get; set; }
+        public Doctor Doctor { get; set; }=default!;
+        public int PatientId { get; set; }
+        public int Rating { get; set; }
+        public string? Comment { get; set; }
+    }
+}
