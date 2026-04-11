0f8cd7d74023d84000b71d39cc2a81b8f0859027
Mariam Hagras <mariamhagras99@gmail.com>
2026-04-11 18:12:25 +0200

    adding refresh token functionality

--- TadaWyFront/src/services/AxiosConfig.js
+++ TadaWyFront/src/services/AxiosConfig.js
@@ -34,4 +34,86 @@ axiosInstance.interceptors.request.use(
   },
   (error) => Promise.reject(error),
 );
+
+let isRefreshing = false;
+let failedQueue = [];
+
+const processQueue = (error, token = null) => {
+  failedQueue.forEach(prom => {
+    if (error) {
+      prom.reject(error);
+    } else {
+      prom.resolve(token);
+    }
+  });
+  failedQueue = [];
+};
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      if (isRefreshing) {
+        return new Promise(function(resolve, reject) {
+          failedQueue.push({ resolve, reject });
+        }).then(token => {
+          originalRequest.headers.Authorization = `Bearer ${token}`;
+          return axiosInstance(originalRequest);
+        }).catch(err => {
+          return Promise.reject(err);
+        });
+      }
+
+      originalRequest._retry = true;
+      isRefreshing = true;
+
+      const refreshToken = TokenService.getRefreshToken() || localStorage.getItem("refreshToken");
+
+      if (!refreshToken) {
+        processQueue(new Error("No refresh token available"));
+        TokenService.removeToken();
+        localStorage.removeItem("userToken");
+        localStorage.removeItem("refreshToken");
+        window.location.href = '/login';
+        return Promise.reject(error);
+      }
+
+      try {
+        const response = await axios.post(`${ENV.API_URL}/Auth/RefreshToken`, {
+          token: TokenService.getToken() || localStorage.getItem("userToken"),
+          refreshToken: refreshToken
+        });
+        
+        const { token, refreshToken: newRefreshToken } = response.data;
+        
+        TokenService.setToken(token, newRefreshToken);
+        localStorage.setItem("userToken", token);
+        if (newRefreshToken) {
+          localStorage.setItem("refreshToken", newRefreshToken);
+        }
+        
+        axiosInstance.defaults.headers.common.Authorization = `Bearer ${token}`;
+        originalRequest.headers.Authorization = `Bearer ${token}`;
+        
+        processQueue(null, token);
+        
+        return axiosInstance(originalRequest);
+      } catch (refreshError) {
+        processQueue(refreshError, null);
+        TokenService.removeToken();
+        localStorage.removeItem("userToken");
+        localStorage.removeItem("refreshToken");
+        window.location.href = '/login';
+        return Promise.reject(refreshError);
+      } finally {
+        isRefreshing = false;
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
 export default axiosInstance;
