97f3a521ded5a1463a765b111f758a61366bc7c0
Ibrahim Sheta <hemasheta061@gmail.com>
2026-04-21 23:40:10 +0200

    DTos Added

---
+++ TadaWy.Applicaation/DTO/ChatDTOs/ConversationDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TadaWy.Applicaation.DTO.ChatDTOs
+{
+    // DTO for Backend sending a conversation inbox list Response to Fronted
+    public class ConversationDto
+    {
+        public string UserId { get; set; } = default!;
+
+        public string FullName { get; set; } = default!;
+
+        public string? ImageUrl { get; set; }
+
+        public string? LastMessage { get; set; }
+
+        public DateTime LastMessageDate { get; set; }
+
+        public bool IsSeen { get; set; }
+    }
+}
